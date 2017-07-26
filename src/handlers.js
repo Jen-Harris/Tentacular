930272844e04a73a039435b027a2275c7202b384
jen-harris <jennifer.louise.harris@gmail.com>
2017-07-26 17:01:59 +0100

    created router.js fixes #4

--- src/handlers.js
+++ src/handlers.js
@@ -2,3 +2,13 @@
       //index.html')
 // 2. handles search --> making a call to request.js and processes the result;
 // 3. handles other and returns 404 error.
+const path = require("path");
+
+
+const contentType = {
+  'index.html': 'text/html',
+  'main.css': 'text/css',
+  'dom.js': 'application/javascript',
+  'clientRequest.js': 'application/javascript',
+  'favicon.ico': 'image/x-icon',
+}[endpoint]
