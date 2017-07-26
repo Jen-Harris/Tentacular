cbeccf90c2e72538bbf5766222e83307b28d92b6
Bart Bucknill <bartbucknill@hotmail.com>
2017-07-26 16:15:13 +0100

    wrote server.js
    #2

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
 //create server(router).listen(port)
+const http = require('http');
+const router = require('./router');
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 4000; 
+
+const server = http.createServer(router);
+
+server.listen(port, () => {
+    console.log(`Server listening on: http://${host}:${port}`);
+});
