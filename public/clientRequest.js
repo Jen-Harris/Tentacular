cfd92e2096ce528e8ddbb179fad1b3165fba8adb
Bart Bucknill <bartbucknill@hotmail.com>
2017-07-26 16:38:51 +0100

    write xmlhttprequest
    #9

--- public/clientRequest.js
+++ public/clientRequest.js
@@ -1,1 +1,14 @@
-//xmlhttprequest
+function serverRequest(url, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        cb(null, xhr.response);
+      } else {
+        cb(xhr.status, xhr.response);
+      }
+    }
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+}
