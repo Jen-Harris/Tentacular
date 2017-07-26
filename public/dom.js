0091bde275b3d6381897d16eff69f69ad90b7b86
Bart Bucknill <bartbucknill@hotmail.com>
2017-07-26 17:23:32 +0100

    wrote some of dom.js
    #10

--- public/dom.js
+++ public/dom.js
@@ -1,3 +1,30 @@
 //on.submit --> get values for language and search input fields;
 //Use values to create queryString
 //call request.js with 2 args: queryString and render callback
+
+//SELECTORS
+var button = document.getElementsByName('submit');
+var searchInput = document.getElementsByName('search')[0];
+var languageInput = document.getElementsByName('language')[0];
+
+//CALLBACKSa & HELPER FUNCTIONS
+function render(data) {
+
+}
+
+function getInput() {
+    var searchData = searchInput.value;
+    var languageData = languageInput.value;
+    return [searchData, languageData];
+}
+
+function makeUrl(userInputArray) {
+   return '/q=' + userInputArray[0] + '&' + userInputArray[1];
+}
+
+// either searchTerm or Language or both could be empty strings
+
+//EVENT LISTENERS
+button.addEventListener('click', function(event) {
+    var queryString = makeUrl(getInput());
+})
