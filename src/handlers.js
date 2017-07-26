7077aee6f3561245d1a1ed4402189d9e61d07b44
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-07-26 15:35:01 +0100

    basic skeleton files created,  fixes #1

---
+++ src/handlers.js
@@ -0,0 +1,4 @@
+// 1. handler to return the homepage('index.html and all resources from
+      //index.html')
+// 2. handles search --> making a call to request.js and processes the result;
+// 3. handles other and returns 404 error.
