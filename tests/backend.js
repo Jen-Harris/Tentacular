a560f8a2691370b0d300b9577a9a9d7bf98459fc
Morkeltry <morkeltry@mailfence.com>
2017-07-26 17:20:42 +0100

    added router & filehandler test markers

--- tests/backend.js
+++ tests/backend.js
@@ -1,11 +1,56 @@
 var test = require ('tape');
+var router = require ('./router');
+var fileHandler = require ('./handlers');
 
 //see this for an example for passing different request objects to router;
 //https://github.com/foundersandcoders/error-handling-workshop/blob/master/acceptance/index.js
 
-//input validation test (for example if it has the correct input);
-//test url maker function
 test ('trivial test', t => {
   t.pass();
   t.end();
 })
+
+//input validation test (for example if it has the correct input);
+//test url maker function
+
+//test handlers.js blocks illegal characters in filename.
+
+test ('trivial test', t => {
+  t.end();
+});
+
+//test handlers.js 404s on directory traverse using ../ /.. // or domain trick with @
+
+test ('trivial test', t => {
+  t.end();
+});
+
+//test handlers.js 404s on known bad filename
+
+test ('trivial test', t => {
+  t.end();
+});
+
+//test correctly formatted API request results in API http request
+
+test ('trivial test', t => {
+  t.end();
+});
+
+//test correctly formatted file request urls result in content-type header
+
+test ('trivial test', t => {
+  t.end();
+});
+
+//
+
+test ('trivial test', t => {
+  t.end();
+});
+
+//
+
+test ('trivial test', t => {
+  t.end();
+});
