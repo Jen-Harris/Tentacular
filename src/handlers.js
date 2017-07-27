1bb64d3d6aa22eb462d79cd34dddecd725fef9ec
jen-harris <jennifer.louise.harris@gmail.com>
2017-07-27 15:03:21 +0100

    responding to Bart's comments and amending the code to fix issues relates PR #18 & issues  #4 #5

--- src/handlers.js
+++ src/handlers.js
@@ -11,7 +11,12 @@ let handlers = module.exports = {}
 
 handlers.serveLanding = (request, response) => {
   fs.readFile(path.join(__dirname, '..', 'public', 'index.html'), (error, file) => {
-    if (error) throw error;
+    if (error) {
+      response.writeHead(500, {
+        'content-type': 'text/html'
+      });
+      response.end("Sorry! We've had a problem!");
+    };
     response.writeHead(200, {
       'content-type': 'text.html'
     });
@@ -20,28 +25,43 @@ handlers.serveLanding = (request, response) => {
 }
 
 handlers.servePublic = (request, response) => {
-  fs.readFile(path.join(__dirname, '..', 'request.url'), (error, file) => {
-    if (error) throw error;
-    let content = request.url.split('.')[1];
-    const contentType = {
-      'index.html': 'text/html',
-      'main.css': 'text/css',
-      'dom.js': 'application/javascript',
-      'clientRequest.js': 'application/javascript',
-      'favicon.ico': 'image/x-icon',
-    }
-    response.writeHead(200, {`content-type: ${contentType[content]}`})
-    response.end(file);
-  });
+  // define whitelist for file endings
+  const url = request.url;
+  const contentType = {
+    '/index.html': 'text/html',
+    '/main.css': 'text/css',
+    '/clientRequest.js': 'application/javascript',
+    '/dom.js': 'application/javascript',
+    '/favicon.ico': 'image/x-icon',
+  }[url];
+  // now if contentType is truthy do this stuff
+  if (contentType) {
+    const filePath = path.join(__dirname, '..', 'public', url);
+    fs.readFile(filePath, (error, file) => {
+      // but if there's error handle that first
+      if(error){
+        response.writeHead(500, {'content-type': 'text/html'});
+        response.end("Sorry! We've had a problem!");
+      } else {
+        // otherwise provide the correct file
+        response.writeHead(200, {`content-type: ${contentType}`})
+        response.end(file);
+      }
+    };
+  };
 }
 
 handlers.handleSearch = (request, response) => {
   let gitInfo = apiRequest(request, response);
-  response.writeHead(200, {'content-type': 'text.html'});
+  response.writeHead(200, {
+    'content-type': 'text/html'
+  });
   response.end(gitInfo);
 }
 
 handlers.pageNotFound = (request, response) => {
-  response.writeHead(404, {'content-type': 'text.html'});
+  response.writeHead(404, {
+    'content-type': 'text/html'
+  });
   response.end();
 }
