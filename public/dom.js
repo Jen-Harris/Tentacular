1ea11faa1d23f675b6debe8706f076c6c9b204fa
Morkeltry <morkeltry@mailfence.com>
2017-07-28 11:39:37 +0100

    added error message for empty results

--- public/dom.js
+++ public/dom.js
@@ -7,8 +7,17 @@ var button = document.getElementsByName('submit')[0];
 var searchInput = document.getElementsByName('search')[0];
 var languageInput = document.getElementsByName('language')[0];
 var resultsField = document.getElementById('js-results');
+var currentSearchTerm = ['',''];
 
 //CALLBACKSa & HELPER FUNCTIONS
+
+function renderError (message) {
+    var errorDiv = document.createElement('div');
+    var errorMessage = document.createTextNode(message);
+    errorDiv.appendChild(errorMessage);
+    resultsField.appendChild(errorDiv);
+}
+
 function render(error, data) {
   // if there is already information on the page then we need to remove it
   if (resultsField.firstElementChild) {
@@ -16,24 +25,30 @@ function render(error, data) {
   }
   // then if there is an error we create an error div
   if (error) {
-    var errorDiv = document.createElement('div');
-    var errorMessage = document.createTextNode("Sorry, we've got issues. Please try again!")
-    errorDiv.appendChild(errorMessage);
-    resultsField.appendChild(errorDiv);
+    renderError ("Sorry, we've got issues. Please try again!");
   } else {
     var parsedData = JSON.parse(data);
 
-    var repoList = parsedData.items.map(function(repo){
-      return '<li><a href = ' +
-        repo.html_url +
-        '><h3>' +
-        repo.full_name +
-        '</h3></a></li>';
-    }).join('');
+    console.log (parsedData.items);
+    console.log (parsedData.items.length);
 
-    var list = '<ul>' + repoList + '</ul>'
-    //... and sorry for this innerHTML too =);
-    resultsField.innerHTML = list;
+    if (!parsedData.items.length) {
+      renderError(`Couldn\'t find any results for ${currentSearchTerm[0]} in ${currentSearchTerm[1]}.\n
+       Try something more realistic.\n Or not. Like, whatevs.`);
+    }
+    else {
+      var repoList = parsedData.items.map(function(repo){
+        return '<li><a href = ' +
+          repo.html_url +
+          '><h3>' +
+          repo.full_name +
+          '</h3></a></li>';
+      }).join('');
+
+      var list = '<ul>' + repoList + '</ul>'
+      //... and sorry for this innerHTML too =);
+      resultsField.innerHTML = list;
+    }
 
   }
 }
@@ -51,6 +66,9 @@ function makeUrl(userInputArray) {
 
 //EVENT LISTENERS
 button.addEventListener('click', function(event) {
-  var queryString = makeUrl(getInput());
+  event.preventDefault();
+  currentSearchTerm = getInput();
+  var queryString = makeUrl(currentSearchTerm);
+
   serverRequest(queryString, render);
 })
